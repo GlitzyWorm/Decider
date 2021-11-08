15271a108376a91b4adfd2e1a1f6b8be6c841f3f
Mathias - s214936 <s214936@student.dtu.dk>
2021-11-08 17:19:18 +0100

    Added App.java to build artifact

---
+++ src/main/java/dk/kattehale/decider/App.java
@@ -0,0 +1,10 @@
+package dk.kattehale.decider;
+
+public class App {
+
+    // Unable to build artifact without this
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+
+}
